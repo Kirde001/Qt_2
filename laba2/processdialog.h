14543ab16706606ad48737d66b66547c8c4f3166
Kirill Poliakov <145561178+Kirde001@users.noreply.github.com>
2025-05-06 01:44:05 +0300

    Update processdialog.h

--- laba2/processdialog.h
+++ laba2/processdialog.h
@@ -1,1 +1,34 @@
+#ifndef PROCESSDIALOG_H
+#define PROCESSDIALOG_H
+
+#include <QDialog>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QLabel>
+
+class ProcessDialog : public QDialog {
+    Q_OBJECT
+
+public:
+    explicit ProcessDialog(QWidget *parent = nullptr);
+    ~ProcessDialog();
+
+signals:
+    void dataProcessedAndReady(const QString &result);
+
+private slots:
+    void onProcessDialogButtonClicked();
+private:
+    QLineEdit *inputField;
+    QComboBox *modeComboBox;
+    QPushButton *processButton;
+    QLabel *resultLabel;
+    QPushButton *okButton;
+    QPushButton *cancelButton;
+    QString processText(const QString &input, const QString &mode);
+};
+
+#endif // PROCESSDIALOG_H
 
