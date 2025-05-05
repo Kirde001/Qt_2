9c1a93839bac8214ef58d89e4c5646442fcea3b2
Kirill Poliakov <145561178+Kirde001@users.noreply.github.com>
2025-05-06 01:44:39 +0300

    Update advanceddataprocessor.h

--- laba2/advanceddataprocessor.h
+++ laba2/advanceddataprocessor.h
@@ -1,1 +1,25 @@
+#ifndef ADVANCED_DATA_PROCESSOR_H
+#define ADVANCED_DATA_PROCESSOR_H
+
+#include <QObject>
+#include <QString>
+
+class AdvancedDataProcessor : public QObject {
+    Q_OBJECT
+
+public:
+    explicit AdvancedDataProcessor(QObject *parent = nullptr);
+
+public slots:
+    void step1_ToUpper(const QString &input);
+    void step2_Reverse(const QString &input);
+    void step3_LogAndFinalize(const QString &input);
+
+signals:
+    void step1Complete(const QString &result);
+    void step2Complete(const QString &result);
+    void processingComplete(const QString &finalResult);
+};
+
+#endif // ADVANCED_DATA_PROCESSOR_H
 
